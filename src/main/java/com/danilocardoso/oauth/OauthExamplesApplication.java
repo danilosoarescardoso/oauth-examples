822473fbcb65e183778121e9591b8885f9037f90
Danilo Cardoso <dsc.soares@gmail.com>
2021-07-17 15:56:54 -0300

    initial commit

---
+++ src/main/java/com/danilocardoso/oauth/OauthExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.danilocardoso.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthExamplesApplication.class, args);
+	}
+
+}
