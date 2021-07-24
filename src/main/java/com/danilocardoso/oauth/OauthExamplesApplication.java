872e9f944d8ecb1c8a2a7e18feb3f2cdeb1d4275
Danilo Cardoso <dsc.soares@gmail.com>
2021-07-24 20:54:46 -0300

    starting oauth configurations

--- src/main/java/com/danilocardoso/oauth/OauthExamplesApplication.java
+++ src/main/java/com/danilocardoso/oauth/OauthExamplesApplication.java
@@ -2,6 +2,7 @@ package com.danilocardoso.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 public class OauthExamplesApplication {
